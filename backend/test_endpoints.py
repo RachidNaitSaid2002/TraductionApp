cd7be045c88cb2055bbda90814195b64e61a88ff
rachidnaitsaid10 <rachidnaitsaid10@gmail.com>
2025-11-29 13:41:34 +0100

    Add test unitaire (test login - test prediction(no token)

---
+++ backend/test_endpoints.py
@@ -0,0 +1,22 @@
+import pytest
+from fastapi.testclient import TestClient
+from main import app, get_uer, verify_password, create_jwt
+
+client = TestClient(app)
+
+
+def test_login():
+
+    response = client.post('/login', json={
+        "email":'testuser@gmail.com',
+        "hashed_password":'tesruserhashedpassword123'
+    })
+
+    assert response.status_code == 200
+    assert response.json() == {'message':'Username ou Password incorrect !!'}
+
+
+def test_prediction_no_token():
+    response = client.post("/Prediction/FnEn", json={"user_text": "Bonjour"})
+
+    assert response.status_code == 403
